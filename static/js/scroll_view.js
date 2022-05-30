408229a557be7da95959df76d0bb7b40b3283eac
Suanlianmang <40178928+Suanlianmang@users.noreply.github.com>
2022-05-30 21:43:34 +0530

    Add static

---
+++ static/js/scroll_view.js
@@ -0,0 +1,25 @@
+function isElementInViewport (el) {
+
+    // Special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
+
+function animateOnView(el, animation){
+    if(isElementInViewport(el)){
+        el.classList.add(animation);
+    }
+    else{
+        el.classList.remove(animation);
+    }
+}
